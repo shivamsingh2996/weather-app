8f99480e2408f844f2166ffbcf448f35483872fb
Shivam singh <111653557+shivamsingh2996@users.noreply.github.com>
2023-03-20 22:53:17 +0530

    no

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const apiKey="6d6bffddf9d058610d091b9109e6011f"
+
+  const [inputCity,setInputCity] = useState("")
+   const [data, setData] = useState({})
+
+  const getWeatherDetails = (cityName) =>{
+  if(!cityName) return
+  const apiURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+ apiKey
+  axios.get(apiURL).then((res)=>{
+    console.log("respons", res.data)
+    setData(res.data)
+
+  }).catch((err) => {
+    console.log("err", err)
+  })
+ }
+  const handleChangeInput = (e) => {
+          setInputCity(e.target.value)
+  }
+  const handleSearch = () => {
+    getWeatherDetails(inputCity)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="col-md-12">
+    <div className="weatherBg">
+      <h1 className="heading">weather app</h1>
+      <div className="d-grid gap-3 col-4 mt-4">
+      <input type="text" className="form-control" onChange={handleChangeInput}/>
+      <button className="btn btn-primary" type="button"
+      onClick={handleSearch}
+      >Search</button>
+      </div>
     </div>
+    {Object.keys(data).length>0 &&
+    <div className="col-md-12 text-center mt-5">
+      <div className="shadow rounded weatherResultBox">
+        <img className="weathericon" src="https://cdn-icons-png.flaticon.com/512/1779/1779940.png"/>
+
+        <h5 className="weatherCity">
+          {data?.name}
+        </h5>
+        <h6 className="weatherTemp">
+          {((data?.main?.temp)-273.1).toFixed(1)}°C</h6>
+      </div>
+    </div>
+ }
+   </div>
   );
-}
+    }
+
 
 export default App;
